b4df85505c94bfabfd6338042fcb70fb76d3778a
Oksana <ksenia.barshchyk@yandex.ru>
2020-04-20 16:57:08 +0300

    added form methods

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 class User:
     def __init__(self, id, username, password):
@@ -15,12 +15,17 @@ class User:
 users = []
 users.append(User(id=1, username='Anthony', password='password'))
 users.append(User(id=2, username='Oksana', password='password'))
-print("users = ", users)
 
-# @app.route('/login')
-# def login():
-#     return render_template('login.html')
-#
-# @app.route('/profile')
-# def profile():
-#     return render_template('profile.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = [x for x in users if x.username = username]
+
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
