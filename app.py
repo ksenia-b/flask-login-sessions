18f635a911540fe79d7a910153eb7e6883409687
Oksana <ksenia.barshchyk@yandex.ru>
2020-04-20 17:07:20 +0300

    secret key

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, session, redirect, url_for, g
 
 app = Flask(__name__)
+app.secret_key = 'somesecretkey123'
 
 class User:
     def __init__(self, id, username, password):
@@ -19,10 +19,18 @@ users.append(User(id=2, username='Oksana', password='password'))
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        session.pop('user_id', None)
+
         username = request.form['username']
         password = request.form['password']
 
-        user = [x for x in users if x.username = username]
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
 
     return render_template('login.html')
 
